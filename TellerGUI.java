21e65e8cb26cb78356cfc1219e8945b413e6ba74
Jxwgame <109953079+Jxwgame@users.noreply.github.com>
2023-03-31 15:42:59 +0700

    Update TellerGUI.java

--- TellerGUI.java
+++ TellerGUI.java
@@ -2,9 +2,10 @@ import java.awt.*;
 import javax.swing.*;
 public class TellerGUI {
     private JFrame fr;
-    private JPanel p;
+    private JPanel p, a;
     private JButton btn1, btn2, btn3;
     private JLabel lb1, lb2;
+    private JTextField tf, tf1;
     public TellerGUI(){
         fr = new JFrame("Teller GUI");
         fr.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -14,13 +15,24 @@ public class TellerGUI {
         lb1 = new JLabel("Balance");
         lb2 = new JLabel("Amount");
         p = new JPanel();
+        a = new JPanel();
+        tf = new JTextField("6000");
+        tf1 = new JTextField();
+        tf.setEditable(false);
+        a.setLayout(new GridLayout(2,2));
+        a.add(lb1); a.add(tf);
+        a.add(lb2); a.add(tf1);
         fr.setLayout(new BorderLayout());
-        fr.add(lb1, BorderLayout.WEST);
-        fr.add(lb2, BorderLayout.WEST);
+//        fr.setLayout(new GridLayout(2,2));
+//        p.add(lb1); p.add(tf);
+//        p.add(lb2); p.add(tf1);
+        fr.add(a, BorderLayout.NORTH);
+        //fr.add(tf1, BorderLayout.EAST);
         p.add(btn1);
         p.add(btn2);
         p.add(btn3);
-        fr.setSize(300, 300);
+        fr.add(p, BorderLayout.SOUTH);
+        fr.pack();
         fr.setResizable(false);
         fr.setVisible(true);
     }
