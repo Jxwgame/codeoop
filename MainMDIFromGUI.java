94f40cca8fa04121098cf4a3da3905d4f2123daa
Jxwgame <109953079+Jxwgame@users.noreply.github.com>
2023-04-06 15:20:26 +0700

    Add files via upload

---
+++ MainMDIFromGUI.java
@@ -0,0 +1,12 @@
+import javax.swing.SwingUtilities;
+import javax.swing.UIManager;
+public class Main {
+    public static void main(String[] args) {
+        try {
+        UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+    } catch (Exception e) {
+    e.printStackTrace();
+}
+SwingUtilities.invokeLater(() -> { MDIFromGUI fr = new MDIFromGUI(); });
+}
+}
