e2bf7d2f7a4b53d427166ecc700e1cc7146e4c91
Jxwgame <109953079+Jxwgame@users.noreply.github.com>
2023-04-21 00:53:03 +0700

    Add files via upload

---
+++ TellerGUI2.java
@@ -0,0 +1,54 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+public class TellerGUI2 extends Account implements ActionListener{
+    private JFrame fr;
+    private JPanel p, a;
+    private JButton btn1, btn2, btn3;
+    private JLabel lb1, lb2;
+    private JTextField tf, tf1;
+    public TellerGUI2(){
+        fr = new JFrame("Teller GUI");
+        fr.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        btn1 = new JButton("Deposit");
+        btn2 = new JButton("Withdraw");
+        btn3 = new JButton("Exit");
+        lb1 = new JLabel("Balance");
+        lb2 = new JLabel("Amount");
+        p = new JPanel();
+        a = new JPanel();
+        tf = new JTextField((int)balance);
+        tf1 = new JTextField();
+        tf.setEditable(false);
+        a.setLayout(new GridLayout(2,2));
+        a.add(lb1); a.add(tf);
+        a.add(lb2); a.add(tf1);
+        fr.setLayout(new BorderLayout());
+//        fr.setLayout(new GridLayout(2,2));
+//        p.add(lb1); p.add(tf);
+//        p.add(lb2); p.add(tf1);
+        fr.add(a, BorderLayout.NORTH);
+        //fr.add(tf1, BorderLayout.EAST);
+        p.add(btn1);
+        p.add(btn2);
+        p.add(btn3);
+        fr.add(p, BorderLayout.SOUTH);
+        fr.pack();
+        btn1.addActionListener(this);
+        btn2.addActionListener(this);
+        btn3.addActionListener(this);
+        fr.setResizable(false);
+        fr.setVisible(true);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent ae) {
+        if(ae.getSource().equals(btn1)){
+            int amount = Integer.parseInt(tf1.getText());
+            
+        }
+        else if(ae.getSource().equals(btn2)){
+            int amount = Integer.parseInt(tf1.getText());
+        }
+    }
+}
