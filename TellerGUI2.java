b9ac7fd9a55769bc72e4284446b68bd51adf1f5f
Jxwgame <109953079+Jxwgame@users.noreply.github.com>
2023-04-21 12:40:39 +0700

    Update TellerGUI2.java

--- TellerGUI2.java
+++ TellerGUI2.java
@@ -1,13 +1,15 @@
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
-public class TellerGUI2 extends Account implements ActionListener{
+public class TellerGUI2 implements ActionListener{
     private JFrame fr;
-    private JPanel p, a;
+    private JPanel p, a, a2;
     private JButton btn1, btn2, btn3;
     private JLabel lb1, lb2;
     private JTextField tf, tf1;
+    private Account ac;
     public TellerGUI2(){
+        ac = new Account(6000, "");
         fr = new JFrame("Teller GUI");
         fr.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         btn1 = new JButton("Deposit");
@@ -17,38 +19,47 @@ public class TellerGUI2 extends Account implements ActionListener{
         lb2 = new JLabel("Amount");
         p = new JPanel();
         a = new JPanel();
-        tf = new JTextField((int)balance);
+        a2 = new JPanel();
+        tf = new JTextField((int) ac.getBalance()+"");
         tf1 = new JTextField();
         tf.setEditable(false);
-        a.setLayout(new GridLayout(2,2));
+        a.setLayout(new GridLayout(1, 2));
         a.add(lb1); a.add(tf);
-        a.add(lb2); a.add(tf1);
-        fr.setLayout(new BorderLayout());
+        a2.setLayout(new GridLayout(1, 2));
+        a2.add(lb2); a2.add(tf1);
+        fr.setLayout(new GridLayout(4, 1));
 //        fr.setLayout(new GridLayout(2,2));
 //        p.add(lb1); p.add(tf);
 //        p.add(lb2); p.add(tf1);
         fr.add(a, BorderLayout.NORTH);
+        fr.add(a2, BorderLayout.SOUTH);
         //fr.add(tf1, BorderLayout.EAST);
         p.add(btn1);
         p.add(btn2);
         p.add(btn3);
-        fr.add(p, BorderLayout.SOUTH);
-        fr.pack();
+        //a2.setLayout(new FlowLayout());
+        fr.add(p, BorderLayout.CENTER);
         btn1.addActionListener(this);
         btn2.addActionListener(this);
         btn3.addActionListener(this);
-        fr.setResizable(false);
+        fr.pack();
         fr.setVisible(true);
     }
 
     @Override
     public void actionPerformed(ActionEvent ae) {
-        if(ae.getSource().equals(btn1)){
+        if(ae.getSource().equals(btn2)){
             int amount = Integer.parseInt(tf1.getText());
-            
+            ac.withdraw(amount);
+            tf.setText(ac.getBalance()+"");
+            }
+        else if(ae.getSource().equals(btn1)){
+            int amount = Integer.parseInt(tf1.getText());
+            int total = (int)ac.getBalance() + amount;
+            tf.setText(total+"");
         }
-        else if(ae.getSource().equals(btn2)){
-            int amount = Integer.parseInt(tf1.getText());
+        else if(ae.getSource().equals(btn3)){
+            System.exit(0);
         }
     }
 }
